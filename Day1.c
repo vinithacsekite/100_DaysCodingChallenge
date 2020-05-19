10e09322c33f74adeb31e80f4938c8047d78c821
vinitha <33887172+vinithacsekite@users.noreply.github.com>
2020-05-19 12:46:34 +0530

    Create Day1.c

---
+++ Day1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int a,b,sum;//variable declaration
+    printf("Enter two numbers to add:\n");
+    scanf("%d %d",&a,&b);
+    sum=a+b;// addition
+    printf("\nTotal is %d",sum);
+  
+  return 0;
+}
