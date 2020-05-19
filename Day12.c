cace0b7795060c371cc4f471e1f29db177d90ae2
vinitha <33887172+vinithacsekite@users.noreply.github.com>
2020-05-19 13:27:10 +0530

    Create Day12.c

---
+++ Day12.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    char var;
+   // printf("\nEnter a character\n");
+    scanf("%c",&var);
+    //printf("\nASCII value of %c is %d",(var+1),var+1);
+        printf("\nASCII value of %c is %d",(var),var);
+  
+  return 0;
+}
