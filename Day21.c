a75e2e7e2587f83e6cc08bc1614c972cfdfeabd0
vinitha <33887172+vinithacsekite@users.noreply.github.com>
2020-05-19 13:36:05 +0530

    Create Day21.c

---
+++ Day21.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  for(int i=1;i<10;i++){
+      if(i==2 || i==5)
+      {
+          printf("\n");
+      }
+      printf("%d",i);
+      
+  }
+  return 0;
+}
