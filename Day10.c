e1d6f9420bd416ca5dd2506bdced44beff801521
vinitha <33887172+vinithacsekite@users.noreply.github.com>
2020-05-19 13:00:30 +0530

    Create Day10.c

---
+++ Day10.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void) {
+    int i,a,b,s=0;
+    printf("\nEnter two numbers to check if Amicable or not\n");
+    scanf("%d\n%d",&a,&b);
+     for(i=1;i<a;i++)
+    {
+        if(a%i==0)
+        {
+            s=s+i;
+        }
+    }
+ 
+    if(s==b)
+    {
+        s=0;
+        for(i=1;i<b;i++)
+        {
+            if(b%i==0)
+            {
+                s=s+i;
+            }
+        }
+ 
+       
+    }
+    
+    if(s==a)
+    {
+        printf("%d and %d are Amicable numbers",a,b);
+    }
+    else
+    {
+        printf("%d and %d are not Amicable numbers",a,b);
+    }
+    
+  return 0;
+}
