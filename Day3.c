802de72fefb38a64bdadab254d67a9881e7cca04
vinitha <33887172+vinithacsekite@users.noreply.github.com>
2020-05-19 12:52:47 +0530

    Create Day3.c

---
+++ Day3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    float a,b;
+    printf("\nEnter 2 values:\n");
+    scanf("%f %f",&a,&b);
+    printf("\nMultiplication result is %f",a*b);
+    printf("\nDivision result is %f",a/b);
+    
+  
+  return 0;
+}
